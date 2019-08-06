776a2a124eaa53716f0c6648a5d9d7948423fef2
Roman Heinrich <roman.heinrich@gmail.com>
2019-08-06 13:25:01 +0200

    first baseline benchmarks

--- mix.exs
+++ mix.exs
@@ -23,7 +23,8 @@ defmodule Calypte.MixProject do
     [
       {:libgraph, github: "bitwalker/libgraph"},
       {:decimal, "~> 1.7"},
-      {:exrun, "~> 0.1"}
+      {:exrun, "~> 0.1", only: [:test, :dev]},
+      {:benchee, "~> 1.0", only: [:test, :dev]},
     ]
   end
 end
